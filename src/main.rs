047bd62e44b4de451fa39376035fdec8ba0c314d
Eddy Ogola <eddy.ogola1@gmail.com>
2022-05-22 19:17:42 -0400

    mode initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
 
-    let mut v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let mut v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 5];
     println!("The average of {:?} is {}", v, average(&v));
 
     // wasn't able to show the array itself, with the median
     // got immutable + mutable borrow error
     println!("The median of the above array is {}", median(&mut v));
+
+    println!("The mode of {:?} is {:?}", v, mode(&v));
 }
 
 // given a list of integers, use a vector and return the mean
@@ -26,4 +30,15 @@ fn median(numbers: &mut [i32]) -> f64 {
     } else {
         return numbers[mid] as f64;
     }
+}
+
+fn mode(numbers: &[i32]) -> i32 {
+    let mut map = HashMap::new();
+
+    for num in numbers {
+        map.entry(num).or_insert(0) += 1;
+    }
+
+    // TODO
+    // get most occuring number by count - map value
 }
